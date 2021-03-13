a4dc10a83b79ca59898c5cb735d8bc6c60490dbc
John Fedora <brukholevin@unknown68ecc56c72fd.attlocal.net>
2021-03-13 18:23:49 -0500

    something resembling a UI

--- main.c
+++ main.c
@@ -35,9 +35,10 @@ bool is_taken(int x1, int y1, int j){
     return false;
 }
 
-void *update_dwarfs() {
+void *update_dwarfs(void *in) {
     while(true) {	
-        clear();
+		WINDOW *win = (WINDOW *)in;
+        wclear(win);
         for (int i = 0; i < n; ++i) {
             struct entity *en = entities[i];
             if (en->mover) {
@@ -61,9 +62,10 @@ void *update_dwarfs() {
                     en->y += diry;
                 }
             }
-            mvaddch(en->y, en->x, en->c);
+            mvwaddch(win, en->y, en->x, en->c);
         }
-        refresh();
+	  	box(win, 0, 0);	
+        wrefresh(win);
         usleep(250000);
 	}
 }
@@ -87,30 +89,42 @@ void initialize_the_boys() {
 
 
 int main(int argc, char *argv[])
-{	WINDOW *my_win;
-	int startx, starty, width, height;
-    srand(time(0));
+{	
+	
+	initscr();// begin ncurses
+	noecho(); // don't echo inputted chars from any getch() calls
+	keypad(stdscr, TRUE); // read keypad input
+	int ch; // character for getting
+	int dwarfpop = 0; // these check if we've popped up any number of flags
+	int applepop = 0;
+	int kgpop = 0; // technically unused
 
-	my_win = initscr();			/* Start curses mode 		*/
-	cbreak();			/* Line buffering disabled, Pass on
-					 * everty thing to me 		*/
-	keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
+	printw("Dwarf Game");
+	printw("\n\n\n\n");
+	printw(" d - current dwarf count\n\n");
+	printw(" a - make an apple\n\n");
+	printw(" k - kg of rock moved\n\n");
+	refresh();			/* Print it on to the real screen */
+	int startx, starty, width, height;
+
+	noecho(); // doesn't print the buttons you press
 
 	height = 3;
 	width = 10;
-	starty = (LINES - height) / 2;	/* Calculating for a center placement */
-	startx = (COLS - width) / 2;	/* of the window		*/
 	//printw("Press F1 to exit");
     curs_set(0);
 	refresh();
+	WINDOW *win = newwin(32, 95, 2, 25); // game window
+  	box(win, 0, 0);	
+	wrefresh(win);
 
     entities = malloc(n*sizeof(struct entity *));
-    getmaxyx(my_win, maxy, maxx);
-    initialize_the_boys(startx, starty);
+    getmaxyx(win, maxy, maxx);
+    initialize_the_boys();
 
     pthread_t dwarf;
 
-    pthread_create(&dwarf, NULL, update_dwarfs, NULL);
+    pthread_create(&dwarf, NULL, update_dwarfs, win);
 
     while (getch() != KEY_F(1));
 		
